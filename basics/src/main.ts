a2eb986f7e50aae3368fef5033d3ef2d149d23b3
Do Thien Hoang <theksbd@gmail.com>
2023-12-31 16:19:04 +0700

    update

--- basics/src/main.ts
+++ basics/src/main.ts
@@ -1,3 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic();
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
