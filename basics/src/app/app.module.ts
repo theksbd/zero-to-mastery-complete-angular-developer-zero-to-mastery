f204261d61bb76ae59c9baa31a9ae55b25a07743
Do Thien Hoang <theksbd@gmail.com>
2024-01-01 20:02:35 +0700

    update

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
