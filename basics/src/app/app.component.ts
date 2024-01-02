a53c225b8417e33c83945a4c4af97f9832648e8c
Do Thien Hoang <thien-hoang@system-exe.com.vn>
2024-01-02 10:06:57 +0700

    update

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name = 'Hoang';
-  imgUrl = 'https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+  imgUrl =
+    'https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
 
-  getName(): void {
+  getName(): string {
     return this.name;
   }
 }
