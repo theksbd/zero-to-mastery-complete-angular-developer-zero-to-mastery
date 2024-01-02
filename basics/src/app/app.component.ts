d8180699d88c0621909894e4255c578827684ea4
Đỗ Thiện Hoàng <theksbd@gmail.com>
2024-01-02 10:01:20 +0700

    Update app.component.ts

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'Hoang';
+  imgUrl = 'https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+
+  getName(): void {
+    return this.name;
+  }
 }
